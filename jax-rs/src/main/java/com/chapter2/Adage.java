ef1398ed4044988ad93e81c7b8a4c4ef7debfc3e
vladosby <vladislavn.kievski@gmail.com>
2017-06-12 00:52:16 +0300

    jax-rs. Adages, xml, json, plain text methods.

---
+++ jax-rs/src/main/java/com/chapter2/Adage.java
@@ -0,0 +1,28 @@
+package com.chapter2;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by vlados on 6/12/2017.
+ */
+
+@XmlRootElement(name = "adage")
+public class Adage {
+    private String words;
+    private int wordCount;
+
+    public Adage() {
+    }
+
+    @Override
+    public String toString() {
+        return words + " -- " + wordCount + " words";
+    }
+    public void setWords(String words) {
+        this.words = words;
+        this.wordCount = words.trim().split("\\s+").length;
+    }
+    public String getWords() { return this.words; }
+    public void setWordCount(int wordCount) { }
+    public int getWordCount() { return this.wordCount; }
+}
---
+++ jax-rs/src/main/java/com/chapter2/Adages.java
@@ -0,0 +1,72 @@
+package com.chapter2;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.namespace.QName;
+import java.util.Random;
+
+/**
+ * Created by vlados on 6/12/2017.
+ */
+@Path("/adages")
+public class Adages {
+    private String[] aphorisms = {"What can be shown cannot be said.",
+            "If a lion could talk, we could not understand him.",
+            "Philosophy is a battle against the bewitchment of " +
+            "our intelligence by means of language.",
+            "Ambition is the death of thought.",
+            "The limits of my language mean the limits of my world."};
+
+    public Adages() {
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_XML})
+    public JAXBElement<Adage> getXml() {
+        return toXml(createAdage());
+    }
+
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    @Path("/json")
+    public String getJson() {
+        return toJson(createAdage());
+    }
+
+    @GET
+    @Produces({MediaType.TEXT_PLAIN})
+    @Path("/plain")
+    public String getPlain() {
+        return createAdage().toString() + "\n";
+    }
+
+    @XmlElementDecl(namespace = "http://aphorism.adage", name = "adage")
+    private JAXBElement<Adage> toXml(Adage adage) {
+        return new JAXBElement<>(new QName("adage"), Adage.class, adage);
+    }
+
+    private Adage createAdage() {
+        Adage adage = new Adage();
+        adage.setWords(aphorisms[new Random().nextInt(aphorisms.length)]);
+        return adage;
+    }
+
+    // Java Adage --> JSON document
+// Jersey provides automatic conversion to JSON using the Jackson
+// libraries. In this example, the conversion is done manually
+// with the Jackson libraries just to indicate how straightforward it is.
+    private String toJson(Adage adage) {
+        String json = "If you see this, there's a problem.";
+        try {
+            json = new ObjectMapper().writeValueAsString(adage);
+        }
+        catch(Exception e) { }
+        return json;
+    }
+}
